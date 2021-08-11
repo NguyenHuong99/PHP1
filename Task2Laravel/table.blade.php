c69c5d2c97d543a7f5306444b67987016af53174
NguyenHuong99 <71129870+NguyenHuong99@users.noreply.github.com>
2021-08-11 19:56:08 +0700

    Task2mini

---
+++ Task2Laravel/table.blade.php
@@ -0,0 +1,69 @@
+{{--Thua ke tu app.blade template--}}
+@extends('layouts.app')
+
+{{--Noi dung trang con--}}
+{{--Dung app.css--}}
+@section('content')
+    {{--Dinh nghia phan noi dung cua trang table--}}
+    <div class="panel-body">
+
+        {{--form nhap table moi--}}
+        <form action="{{url('table')}}" method="post" class="form-horizontal">
+            {{csrf_field()}}
+
+            {{--Ten table--}}
+            <div class="form-group">
+                <label for="table" class="col-sm-3 control-label">Table</label>
+                <div class="col-sm-6">
+                    <input type="text" name="name" id="table-name" class="form-control">
+                </div>
+            </div>
+
+            {{--Nut Table--}}
+            <div class="form-group">
+                <div class="col-sm-offset-3 col-sm-6">
+                    <button type="submit" class="btn btn-default">
+                        <i class="fa fa-plus"></i>Add Table
+                    </button>
+                </div>
+            </div>
+        </form>
+        {{--Hien thi table hien tai co trong database--}}
+        @if(count($tables) > 0)
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    Current Table
+                </div>
+                <div class="panel-body">
+                    <table class="table table-striped task-table">
+                        {{--Tao tieu de cua cac cot--}}
+                        <thead>
+                        <td>Table</td>
+                        <td>&nbsp</td>
+                        </thead>
+                        {{--Tao cac dong de hien thi noi dung--}}
+                        <tbody>
+                            @foreach($tables as $table)
+                                <tr>
+                                    {{--Hien thi ten table--}}
+                                    <td class="table-text">
+                                        <div>{{$table->name}}</div>
+                                    </td>
+                                    {{--Them nut xoa--}}
+                                    <td>
+                                        <form action="table/{{$table->id}}" method="post">
+                                            {{csrf_field()}}
+                                            {{method_field('DELETE')}}
+                                            <button>Delete Task</button>
+                                            <input type="hidden" name="_method" id="table-id" value="DELETE">
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        @endif
+    </div>
+@endsection
